38336fac81ae1a584ac0a52a866a1a0f35dc376d
JauniKapauni <riches-goods.54@icloud.com>
2026-05-18 15:44:58 +0200

    init: initialize project

---
+++ src/main/java/de/jaunikapauni/axtravel/AxTravel.java
@@ -0,0 +1,16 @@
+package de.jaunikapauni.axtravel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxTravel extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
